1cd2e13206790a7697b86fdfb9caad6561eca3ec
Brugoski <thomasb0831@outlook.com>
2024-09-07 19:43:17 -0400

    Update premake5.lua
    Updated premake

--- premake5.lua
+++ premake5.lua
@@ -15,7 +15,12 @@ project "GLFW"
         "src/input.c",
         "src/monitor.c",
         "src/vulkan.c",
-        "src/window.c"
+        "src/window.c",
+        "src/platform.c",
+        "src/null_init.c",
+        "src/null_monitor.c",
+        "src/null_window.c",
+        "src/null_joystick.c"
     }
     
 	filter "system:windows"
@@ -33,7 +38,8 @@ project "GLFW"
             "src/win32_window.c",
             "src/wgl_context.c",
             "src/egl_context.c",
-            "src/osmesa_context.c"
+            "src/osmesa_context.c",
+            "src/win32_module.c"
         }
 
 		defines 
@@ -41,5 +47,13 @@ project "GLFW"
             "_GLFW_WIN32",
             "_CRT_SECURE_NO_WARNINGS"
 		}
+
+    filter { "system:windows", "configurations:Debug" }
+        buildoptions "/MTd"
+        runtime "Debug"
+
     filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
+
+    filter { "system:windows", "configurations:Dist" }
         buildoptions "/MT"
